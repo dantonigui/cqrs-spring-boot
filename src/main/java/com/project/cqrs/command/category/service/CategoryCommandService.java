6a98f47a9065d89177603ccb7336d50798c8337e
dantonigui <dantonioficial@gmail.com>
2026-05-19 11:07:08 -0300

    feat(category): add CategoryCommandService with create, update and delete

--- src/main/java/com/project/cqrs/command/category/service/CategoryCommandService.java
+++ src/main/java/com/project/cqrs/command/category/service/CategoryCommandService.java
@@ -2,9 +2,9 @@ package com.project.cqrs.command.category.service;
 
 import com.project.cqrs.command.category.dto.request.CreateCategoryRequestDTO;
 import com.project.cqrs.command.category.dto.request.UpdateCategoryRequestDTO;
-import com.project.cqrs.command.category.event.CategoryCreateEvent;
-import com.project.cqrs.command.category.event.CategoryDeleteEvent;
-import com.project.cqrs.command.category.event.CategoryUpdateEvent;
+import com.project.cqrs.shared.event.category.CategoryCreateEvent;
+import com.project.cqrs.shared.event.category.CategoryDeleteEvent;
+import com.project.cqrs.shared.event.category.CategoryUpdateEvent;
 import com.project.cqrs.command.category.kafka.producer.CategoryEventProducer;
 import com.project.cqrs.command.category.model.CategoryCommandEntity;
 import com.project.cqrs.command.category.repository.CategoryRepository;
