644c3290148e4a3aa1b93c4dc84469d8526a1f6c
dantonigui <dantonioficial@gmail.com>
2026-05-19 11:05:37 -0300

    feat(user): add CreateUserRequestDTO and UserQueryDTO

---
+++ src/main/java/com/project/cqrs/command/auth/dto/CreateUserRequestDTO.java
@@ -0,0 +1,28 @@
+package com.project.cqrs.command.auth.dto;
+
+import com.project.cqrs.command.auth.model.UserRole;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record CreateUserRequestDTO(
+
+        @NotBlank
+        @Size(min = 3)
+        String userName,
+
+        @NotBlank
+        @Email
+        String userEmail,
+
+        @NotBlank
+        String userPictureUrl,
+
+        @NotBlank
+        String userGoogleId,
+
+        @NotBlank
+        UserRole userRole
+) {
+
+}
---
+++ src/main/java/com/project/cqrs/query/auth/dto/UserQueryDTO.java
@@ -0,0 +1,34 @@
+package com.project.cqrs.query.auth.dto;
+
+import com.project.cqrs.command.auth.model.UserRole;
+import com.project.cqrs.query.auth.model.UserQueryEntity;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record UserQueryDTO(
+
+        @NotNull
+        Long userId,
+
+        @NotBlank
+        String userName,
+
+        @NotBlank
+        @Email
+        String userEmail,
+
+        @NotBlank
+        UserRole userRole
+) {
+    public static UserQueryDTO from (UserQueryEntity userQueryEntity) {
+        return new UserQueryDTO(
+                userQueryEntity.getId(),
+                userQueryEntity.getUserName(),
+                userQueryEntity.getUserEmail(),
+                userQueryEntity.getUserRole()
+        );
+    }
+}
+
