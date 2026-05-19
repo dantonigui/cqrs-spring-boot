1cbf57f992a0931a16ee31189082787ccbd05623
dantonigui <dantonioficial@gmail.com>
2026-05-19 10:57:21 -0300

    feat(auth): add AuthCommandController with logout endpoint

---
+++ src/main/java/com/project/cqrs/command/auth/controller/AuthController.java
@@ -0,0 +1,34 @@
+package com.project.cqrs.command.auth.controller;
+
+import com.project.cqrs.command.auth.infra.cookie.CookieTokenUtil;
+import com.project.cqrs.command.auth.infra.kafka.UserEventProducer;
+import com.project.cqrs.shared.event.user.UserLogoutEvent;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/api/v1/command/auth")
+public class AuthController {
+
+    private final CookieTokenUtil cookieTokenUtil;
+    private final UserEventProducer userEventProducer;
+
+    public AuthController(CookieTokenUtil cookieTokenUtil, UserEventProducer userEventProducer) {
+        this.cookieTokenUtil = cookieTokenUtil;
+        this.userEventProducer = userEventProducer;
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(HttpServletResponse response, Principal principal) {
+        cookieTokenUtil.clearToken(response);
+
+        Long userId = Long.parseLong(principal.getName());
+        userEventProducer.publishUserLogoutEvent(UserLogoutEvent.logoutEvent(userId));
+        return ResponseEntity.noContent().build();
+    }
+
+}
