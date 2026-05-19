bc67f4cfa0f47cb2af00c0c774df9c3e27669cb2
dantonigui <dantonioficial@gmail.com>
2026-05-19 10:55:16 -0300

    feat(auth): add OAuth2 login with Google and JWT cookie

---
+++ src/main/java/com/project/cqrs/command/auth/infra/security/OAuth2AuthSucessHandler.java
@@ -0,0 +1,44 @@
+package com.project.cqrs.command.auth.infra.security;
+
+import com.project.cqrs.command.auth.infra.cookie.CookieTokenUtil;
+import com.project.cqrs.command.auth.model.UserCommandEntity;
+import com.project.cqrs.command.auth.repository.UserCommandRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+
+import java.io.IOException;
+
+public class OAuth2AuthSucessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private final JwtTokenService jwtTokenService;
+    private final UserCommandRepository userCommandRepository;
+    private final CookieTokenUtil cookieTokenUtil;
+
+    public OAuth2AuthSucessHandler(JwtTokenService jwtTokenService, UserCommandRepository userCommandRepository, CookieTokenUtil cookieTokenUtil) {
+        this.jwtTokenService = jwtTokenService;
+        this.userCommandRepository = userCommandRepository;
+        this.cookieTokenUtil = cookieTokenUtil;
+    }
+
+    @Value("${app.frontend-url:http://localhost:3000}")
+    private String frontendUrl;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
+        var oAuth2User = (DefaultOAuth2User) authentication.getPrincipal();
+        String googleId = oAuth2User.getAttribute("sub");
+
+        UserCommandEntity  userCommandEntity = userCommandRepository.findByGoogleId(googleId)
+                .orElseThrow(() -> new IllegalStateException("User not found with googleId: " + googleId));
+
+        String jwt = jwtTokenService.generateToken(userCommandEntity);
+
+        cookieTokenUtil.writeToken(response, jwt);
+
+        getRedirectStrategy().sendRedirect(request, response, frontendUrl + "/");
+    }
+}
