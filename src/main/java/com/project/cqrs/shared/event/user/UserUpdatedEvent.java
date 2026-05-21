d303fda2bbd2e2b2078bd27f32d906573a850003
dantonigui <dantonioficial@gmail.com>
2026-05-21 19:23:29 -0300

    feat(user/event): add UserUpdatedEvent with factory method

--- src/main/java/com/project/cqrs/shared/event/user/UserUpdatedEvent.java
+++ src/main/java/com/project/cqrs/shared/event/user/UserUpdatedEvent.java
@@ -4,21 +4,35 @@ import com.project.cqrs.command.auth.model.UserCommandEntity;
 
 public final class UserUpdatedEvent extends UserEvent {
 
-    private final String email;
+    private  String email;
 
-    private UserUpdatedEvent(Long userId, String email) {
+    private String name;
+
+
+    public UserUpdatedEvent() {
+        super();
+    }
+
+    private UserUpdatedEvent(Long userId, String email, String name) {
         super(userId);
         this.email = email;
+        this.name = name;
     }
 
     public static UserUpdatedEvent userUpdatedEvent(UserCommandEntity userEntity) {
         return new UserUpdatedEvent(
                 userEntity.getUserId(),
-                userEntity.getUserEmail()
+                userEntity.getUserEmail(),
+                userEntity.getUserName()
         );
     }
 
     public String getEmail() {
         return email;
     }
+
+    public String getName() {
+        return name;
+    }
+
 }
