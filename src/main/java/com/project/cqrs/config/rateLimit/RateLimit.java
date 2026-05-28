5d5335868883e858bfbd976171f26182c267ff6c
dantonigui <dantonioficial@gmail.com>
2026-05-28 17:18:00 -0300

    feat(rate-limit): add custom rate limiting annotation and aspect

---
+++ src/main/java/com/project/cqrs/config/rateLimit/RateLimit.java
@@ -0,0 +1,13 @@
+package com.project.cqrs.config.rateLimit;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface RateLimit {
+    int requests() default 10;
+    int durationSeconds() default 60;
+}
