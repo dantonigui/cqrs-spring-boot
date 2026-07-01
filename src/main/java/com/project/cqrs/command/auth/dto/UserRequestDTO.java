c39637f703d3ba8db6b6852cb83fa0cf45c292b0
dantonigui <dantonioficial@gmail.com>
2026-07-01 15:04:26 -0300

    refactor(auth): update authentication request payloads

--- src/main/java/com/project/cqrs/command/auth/dto/UserRequestDTO.java
+++ src/main/java/com/project/cqrs/command/auth/dto/UserRequestDTO.java
@@ -2,6 +2,7 @@ package com.project.cqrs.command.auth.dto;
 
 import com.project.cqrs.command.auth.model.UserCommandEntity;
 import com.project.cqrs.command.auth.model.UserRole;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
@@ -11,6 +12,7 @@ public record UserRequestDTO(
         Long userId,
 
         @NotBlank
+        @Email
         String userEmail,
 
         @NotBlank
