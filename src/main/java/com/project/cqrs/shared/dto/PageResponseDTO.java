51770546b2a2b95a9923d2c1d8d28d66c6cd1665
dantonigui <dantonioficial@gmail.com>
2026-07-27 22:57:42 -0300

    fix: replace PageImpl caching response with custom page dto

--- src/main/java/com/project/cqrs/shared/dto/PageResponseDTO.java
+++ src/main/java/com/project/cqrs/shared/dto/PageResponseDTO.java
@@ -1,14 +1,32 @@
 package com.project.cqrs.shared.dto;
 
+import org.springframework.data.domain.Page;
+
 import java.util.List;
 
-public record PageResponseDTO<T> (
-    List<T> content,
-    int page,
-    int size,
-    long totalElements,
-    int totalPages,
-    boolean first,
-    boolean last
-) {}
+public record PageResponseDTO<T>(
+        List<T> content,
+        int page,
+        int size,
+        long totalElements,
+        int totalPages,
+        boolean first,
+        boolean last
+) {
 
+    public boolean isEmpty() {
+        return content.isEmpty();
+    }
+
+    public static <T> PageResponseDTO<T> from(Page<T> page) {
+        return new PageResponseDTO<>(
+                page.getContent(),
+                page.getNumber(),
+                page.getSize(),
+                page.getTotalElements(),
+                page.getTotalPages(),
+                page.isFirst(),
+                page.isLast()
+        );
+    }
+}
