bc38ad241bf33939a29610449171816e4aebe059
dantonigui <dantonioficial@gmail.com>
2026-07-30 17:54:49 -0300

    feat(payment): add PaymentRepository with findByMpPaymentId and findPendingPixByOrderId

--- src/main/java/com/project/cqrs/command/payment/repository/PaymentCommandRepository.java
+++ src/main/java/com/project/cqrs/command/payment/repository/PaymentCommandRepository.java
@@ -1,13 +1,23 @@
 package com.project.cqrs.command.payment.repository;
 
 import com.project.cqrs.command.payment.model.PaymentCommandEntity;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface PaymentCommandRepository extends JpaRepository<PaymentCommandEntity,Long> {
 
-    Optional<PaymentCommandEntity> findPendingPixByOrderId(Long orderId);
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("""
+    SELECT o
+    FROM OrderCommandEntity o
+    WHERE o.orderId = :orderId
+""")
+    Optional<PaymentCommandEntity> findPendingPixByOrderId(@Param("orderId") Long orderId);
 
     Optional<PaymentCommandEntity> findByMpPaymentId(String mpPaymentId);
 }
